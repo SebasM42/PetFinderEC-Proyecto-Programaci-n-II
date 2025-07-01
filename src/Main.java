57ddc61ba400989d89c1b2766acbf05887b2d65f
SebasM42 <sebas121024m@gmail.com>
2025-06-30 23:40:38 -0500

    Correccion main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) {
         SistemaBusquedaMascotas sistema = new SistemaBusquedaMascotas();
-
         SwingUtilities.invokeLater(() -> new VentanaInicio(sistema));
     }
 }
