ada010b22cdbc6f0d576e323ec53f031576a48c8
CarlosGoprogra <carlosagh2005@gmail.com>
2025-07-03 14:24:29 -0500

    Validación en el constructor para evitar valores nulos o vacíos en nombres en la clase mascota

--- src/Mascota.java
+++ src/Mascota.java
@@ -6,6 +6,9 @@ public class Mascota {
     private String tamanio;
 
     public Mascota(String nombre, String tipo, String raza, String color, String tamanio) {
+        if (nombre == null || nombre.isEmpty() || tipo == null || tipo.isEmpty()) {
+            throw new IllegalArgumentException("Nombre y tipo no pueden ser nulos o vacíos");
+        }
         this.nombre = nombre;
         this.tipo = tipo;
         this.raza = raza;
@@ -13,24 +16,37 @@ public class Mascota {
         this.tamanio = tamanio;
     }
 
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public String getRaza() {
-        return raza;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getTamanio() {
-        return tamanio;
-    }
+    public String getNombre() { return nombre; }
+    public String getTipo() { return tipo; }
+    public String getRaza() { return raza; }
+    public String getColor() { return color; }
+    public String getTamanio() { return tamanio; }
 
     public String getDescripcion() {
-        return nombre + " - " + tipo + " - " + raza + " - " + color + " - " + tamanio;
+        return "Mascota{" +
+                "nombre='" + nombre + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", raza='" + raza + '\'' +
+                ", color='" + color + '\'' +
+                ", tamanio='" + tamanio + '\'' +
+                '}';
+    }
+
+    @Override
+    public String toString() {
+        return getDescripcion();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mascota mascota = (Mascota) o;
+        return nombre.equals(mascota.nombre) && tipo.equals(mascota.tipo);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(nombre, tipo);
     }
 }
